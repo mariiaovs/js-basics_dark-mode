4c99ebe5fdfe834e071c66d2f74f1b273f60f3c3
Mariia Posselt <maruw@ya.ru>
2023-12-13 19:27:56 +0100

    dark, light and toggle buttons

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkButton = document.querySelector('[data-js="dark-mode-button"]');
+
+const lightButton = document.querySelector('[data-js="light-mode-button"]');
+
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+darkButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+  console.log("click");
+});
+
+lightButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
